32f26b1f989371c9bfe1b0b4a78b24bb07946fd2
myth-faceless <mythmanish743@gmail.com>
2024-12-28 19:47:30 +0545

    bug fixed for invalid OTP

--- backend/src/constants/message.constants.js
+++ backend/src/constants/message.constants.js
@@ -1,67 +1,64 @@
 export const STATUS_CODES = {
-    SUCCESS: 200,
-    CREATED: 201,
-    ACCEPTED: 202,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    PAYMENT_REQUIRED: 402,
-    FORBIDDEN: 403,
-    NOT_FOUND: 404,
-    REQUEST_TIMEOUT: 408,
-    DUPLICATE_ENTRY: 409,
-    TOO_MANY_REQUESTS: 429,
-    INTERNAL_SERVER_ERROR: 500,
-    BAD_GATEWAY: 502,
-    SERVICE_UNAVAILABLE: 503,
-  };
-  
-  export const ERROR_MESSAGES = {
-    INTERNAL_SERVER_ERROR: "Internal Server Error.",
-    UNAUTHORIZED: "Access denied. Please login to continue.",
-    USER_NOT_AUTHORIZED: "User not authorized to perform this action.",
-    FORBIDDEN: "You don't have enough permission to perform this action.",
-    USER_EMAIL_ALREADY_EXIST:
-      "User with this email already exist. Please try with other email.",
-    USER_PHONE_ALREADY_EXIST:
-      "User with this phone already exist. Please try with other phone.",
+  SUCCESS: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  PAYMENT_REQUIRED: 402,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  REQUEST_TIMEOUT: 408,
+  DUPLICATE_ENTRY: 409,
+  TOO_MANY_REQUESTS: 429,
+  INTERNAL_SERVER_ERROR: 500,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+};
 
-    ERROR_CREATING_USER: "Error Registering User.",
-    ERROR_UPDATING_USER: "Error Updating User.",
-    ERROR_DELETING_USER: "Error deleting User.",
-    
-  
-    INVALID_EMAIL_PASSWORD: "Invalid Email, Phone or password.",
-    INCORRECT_EMAIL_PASSWORD: "Incorrect Email or password.",
-    REQUIRED_EMAIL_PASSWORD: "Email and Password is Required",
-  
-    USER_NOT_FOUND: "User not found.",
-    USER_NOT_FOUND_INVALID_TOKEN: "User not found or invalid token.",
-    INVALID_OLD_PASSWORD: "Old Password doesn't match or invalid.",
-    INVALID_TOKEN: "Invalid Token.",
-    SAME_PASSWORD: "New password cannot be the same as old password.",
-  
-    CLOUDINARY_UPLOAD_FAILED: "Failed to upload to cloudinary",
-    CLOUDINARY_AVATAR_UPLOAD_FAILED: "Failed to upload avatar to cloudinary",
-  
-    TOKEN_GENEREATION: "Something went wrong while generating access token",
-  
-    INVALID_REQUEST_BODY: "Invalid request Body.",
+export const ERROR_MESSAGES = {
+  INTERNAL_SERVER_ERROR: "Internal Server Error.",
+  UNAUTHORIZED: "Access denied. Please login to continue.",
+  USER_NOT_AUTHORIZED: "User not authorized to perform this action.",
+  FORBIDDEN: "You don't have enough permission to perform this action.",
+  USER_EMAIL_ALREADY_EXIST:
+    "User with this email already exist. Please try with other email.",
+  USER_PHONE_ALREADY_EXIST:
+    "User with this phone already exist. Please try with other phone.",
 
-    SESSION_EXPIRED: "Session expired. Please log in again.",
-    EMAIL_NOT_VERIFIED: "Email not verified, please verify your email !",
-    REQUIRED_EMAIL_AND_OTP: "Required email and OTP !",
-    INVALID_OR_EXPIRED_OTP: "OTP invalid or expired"
+  ERROR_CREATING_USER: "Error Registering User.",
+  ERROR_UPDATING_USER: "Error Updating User.",
+  ERROR_DELETING_USER: "Error deleting User.",
 
-  };
-  
-  export const SUCCESS_MESSAGES = {
-    USER_REGISTERED: "User registered successfully.",
-    USER_LOGGED_IN: "User logged in successfully.",
-    USER_LOGGED_OUT: "User logged out successfully.",
-    USER_UPDATED: "User updated successfully.",
-    USER_DELETED: "User deleted successfully.",
-    
-  
-    PASSWORD_CHANGED: "Password changed successfully.",
-  
-  };
+  INVALID_EMAIL_PASSWORD: "Invalid Email, Phone or password.",
+  INCORRECT_EMAIL_PASSWORD: "Incorrect Email or password.",
+  REQUIRED_EMAIL_PASSWORD: "Email and Password is Required",
+
+  USER_NOT_FOUND: "User not found.",
+  USER_NOT_FOUND_INVALID_TOKEN: "User not found or invalid token.",
+  INVALID_OLD_PASSWORD: "Old Password doesn't match or invalid.",
+  INVALID_TOKEN: "Invalid Token.",
+  SAME_PASSWORD: "New password cannot be the same as old password.",
+
+  CLOUDINARY_UPLOAD_FAILED: "Failed to upload to cloudinary",
+  CLOUDINARY_AVATAR_UPLOAD_FAILED: "Failed to upload avatar to cloudinary",
+
+  TOKEN_GENEREATION: "Something went wrong while generating access token",
+
+  INVALID_REQUEST_BODY: "Invalid request Body.",
+
+  SESSION_EXPIRED: "Session expired. Please log in again.",
+  EMAIL_NOT_VERIFIED: "Email not verified, please verify your email !",
+  REQUIRED_EMAIL_AND_OTP: "Required email and OTP !",
+  INVALID_OR_EXPIRED_OTP: "OTP invalid or expired",
+  INVALID_OTP: "Invalid OTP, check your OPT again !",
+};
+
+export const SUCCESS_MESSAGES = {
+  USER_REGISTERED: "User registered successfully.",
+  USER_LOGGED_IN: "User logged in successfully.",
+  USER_LOGGED_OUT: "User logged out successfully.",
+  USER_UPDATED: "User updated successfully.",
+  USER_DELETED: "User deleted successfully.",
+
+  PASSWORD_CHANGED: "Password changed successfully.",
+};
