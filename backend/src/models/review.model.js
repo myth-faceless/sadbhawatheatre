068ae3f4cd4116c81c0eb18f787c0849330da0c8
myth-faceless <mythmanish743@gmail.com>
2025-05-21 16:44:19 +0545

    added review model

---
+++ backend/src/models/review.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    drama: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Drama",
+      required: true,
+    },
+    booking: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      requird: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      maxlength: 1000,
+    },
+  },
+  { timestamps: true }
+);
+
+//allows user to give only one review
+reviewSchema.index({ user: 1, drama: 1 }, { unique: true });
+
+export const Review = mongoose.model("Review", reviewSchema);
