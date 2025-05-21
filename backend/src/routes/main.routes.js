69461fa0f1c1cadfbbc49db5eb1958b6fdf83f84
myth-faceless <mythmanish743@gmail.com>
2025-05-21 17:37:40 +0545

    added admin routes to main route

--- backend/src/routes/main.routes.js
+++ backend/src/routes/main.routes.js
@@ -3,11 +3,11 @@ const router = Router();
 
 //router import
 import { userRoutes } from "./user.routes.js";
-// import { adminRoutes } from "./admin.routes.js";
+import { adminRoutes } from "./admin.routes.js";
 
 //routes declaration
 router.use("/", userRoutes);
 
-// router.use("/admin", adminRoutes)
+router.use("/admin/", adminRoutes);
 
 export { router as mainRoutes };
