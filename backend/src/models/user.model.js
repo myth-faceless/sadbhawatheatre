944a8b7446b76a6707f02d563882ee84c09dbfa8
myth-faceless <mythmanish743@gmail.com>
2025-05-21 19:02:35 +0545

    added pending email for email update

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -39,6 +39,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    pendingEmail: {
+      type: String,
+      default: null,
+    },
     otp: {
       type: String,
       default: null,
