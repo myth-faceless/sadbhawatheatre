db4394764668bf7d94bbe993a00c64e84a100872
myth-faceless <mythmanish743@gmail.com>
2025-05-21 17:37:59 +0545

    modified admin validation

--- backend/src/validations/admin.validation.js
+++ backend/src/validations/admin.validation.js
@@ -1,20 +1,5 @@
 import Joi from "joi";
 
-export const registerAdminSchema = Joi.object({
-  fullName: Joi.string().max(100).required(),
-  phoneNumber: Joi.string()
-    .pattern(/^9[0-9]{9}$/)
-    .max(15)
-    .required(),
-  email: Joi.string()
-    .pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
-    .required(),
-  password: Joi.string()
-    .pattern(/^[a-zA-Z0-9]{3,30}$/)
-    .required(),
-  avatar: Joi.any(),
-});
-
 export const loginAdminSchema = Joi.object({
   email: Joi.string()
     .pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
@@ -34,6 +19,12 @@ export const changeAdminPasswordSchema = Joi.object({
   oldPassword: Joi.string()
     .pattern(/^[a-zA-Z0-9]{3,30}$/)
     .required(),
+  newPassword: Joi.string()
+    .pattern(/^[a-zA-Z0-9]{3,30}$/)
+    .required(),
+});
+
+export const resetAdminPasswordSchema = Joi.object({
   newPassword: Joi.string()
     .pattern(/^[a-zA-Z0-9]{3,30}$/)
     .required(),
