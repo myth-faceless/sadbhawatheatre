6b8742324423034aa3f7a26dbb38037ec9bb1d33
myth-faceless <mythmanish743@gmail.com>
2025-05-21 10:37:43 +0545

    continued

--- backend/src/constants/app.constants.js
+++ backend/src/constants/app.constants.js
@@ -1,14 +1,7 @@
 export const PRODUCTION_DB_NAME = "sadbhawa_db";
 export const DEVELOPMENT_DB_NAME = "sadbhawalLocal_db";
 
-export const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:3000",
-  "http://localhost:8081",
-  "http://localhost:9000",
-  "https://abc.com",
-];
+export const allowedOrigins = ["http://localhost:3000"];
 
 export const USER_ICON =
   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
