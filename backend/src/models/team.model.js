21a6a6f838f610a9ce71cd71d1c3e8142722f05b
myth-faceless <mythmanish743@gmail.com>
2025-05-21 16:49:38 +0545

    added necessary variables

--- backend/src/models/team.model.js
+++ backend/src/models/team.model.js
@@ -1,5 +1,34 @@
 import mongoose from "mongoose";
 
-const teamSchema = new mongoose.Schema({}, { timestamps: true });
+const teamSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    bio: { type: String },
+    photo: {
+      url: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      },
+    },
+    socialLinks: {
+      facebook: String,
+      twitter: String,
+      instagram: String,
+      linkedin: String,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Team = mongoose.model("Team", teamSchema);
