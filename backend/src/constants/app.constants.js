018e1eafa6d93c918e8cab83846536f7994dad6e
myth-faceless <mythmanish743@gmail.com>
2025-05-25 19:55:36 +0545

    added necessary messages

--- backend/src/constants/app.constants.js
+++ backend/src/constants/app.constants.js
@@ -5,3 +5,6 @@ export const allowedOrigins = ["http://localhost:3000"];
 
 export const USER_ICON =
   "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+
+export const DEFAULT_ICON =
+  "https://cdn.pixabay.com/photo/2017/03/21/02/00/image-2160911_1280.png";
